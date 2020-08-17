f81e13b85a1b0467ba7c3703607299afe7289866
JeongilSon <xmasdeer2@gmail.com>
2020-08-17 13:27:50 +0900

    fgfg

---
+++ RotTest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float walkSpeed;
+    [SerializeField] float turnSpeed;
+
+    private Transform target;
+    private NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        nav.SetDestination(target.position);
+    }
+}
