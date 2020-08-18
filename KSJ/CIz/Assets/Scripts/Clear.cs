4e175972e1200236982233d10d981e9f4b3b6a16
Haps157 <69827171+Haps157@users.noreply.github.com>
2020-08-18 15:19:58 +0900

    Update
    자료 추가 수정

---
+++ KSJ/CIz/Assets/Scripts/Clear.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Clear : MonoBehaviour
+{
+    GameObject Goal;
+    public GameObject Now;
+    public GameObject After;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    Goal = GameObject.FindWithTag("Goal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll){
+        if(coll.tag=="Cam"){
+            Now.SetActive(false);
+            After.SetActive(true);
+        }
+    }
+}
