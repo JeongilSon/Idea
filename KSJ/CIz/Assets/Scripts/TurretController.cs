119586ba0a5b998abf519ee4292d0023d2c4e852
Haps157 <69827171+Haps157@users.noreply.github.com>
2020-08-18 11:39:07 +0900

    KSU Update
    프로젝트 개발을 위한 개인 폴더

---
+++ KSJ/CIz/Assets/Scripts/TurretController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    //총알 생성 주기
+    public float spawnRateMin = 0.6f;
+    public float spawnRateMax = 2f;
+
+    private Transform target;
+    private float spawnRate;
+    private float timeAfterSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+        target = FindObjectOfType<PlayerController>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeAfterSpawn += Time.deltaTime;
+
+        if(timeAfterSpawn >= spawnRate)
+        {
+            //발사 주기 초기화
+            timeAfterSpawn = 0f;
+            spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+            //총알 생성
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.transform.LookAt(target);
+        }
+    }
+}
