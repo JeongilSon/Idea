0e6cdc7d4bd259495de0e71c91d7430331b7beca
JeongilSon <xmasdeer2@gmail.com>
2020-08-28 16:10:08 +0900

    1차 VR 빌드완료본

---
+++ SON/New Unity Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine.PlayerLoop;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance;
+	public GameObject video;    
+	public AudioClip firstBgmSource, secondBgmSource, thirdBgmSource;
+	public AudioClip keyboardSource;
+	public VideoClip cizLogo, ideaLogo;
+	public VideoClip outro;
+	public int stageCount = 0;
+	public bool textScene;
+	string story = "";
+	Text txt;
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);		
+	}
+	void Start()
+	{
+		txt = GetComponentInChildren<Text>();
+		StartCoroutine(VideoPlay(stageCount));
+	}
+    private void Update()
+    {
+		StageClear();
+	}
+	IEnumerator VideoPlay(int stageCount)
+    {
+		video.SetActive(true);
+		if (stageCount == 0) // 첫 시작 씬
+		{
+			VideoManager.instance.VIdeoSIngle(cizLogo);
+			yield return new WaitForSeconds(2f);
+			VideoManager.instance.VIdeoSIngle(ideaLogo);
+			yield return new WaitForSeconds(2f);
+			VideoManager.instance.player.clip = null;
+			video.SetActive(false);
+			Text(stageCount);
+		}
+	}
+    public void StageClear()
+    {
+        if (gravity.clearCheck == true && stageCount < 3)
+        {
+			SceneManager.LoadScene(0);
+			Text(stageCount);
+			stageCount++;
+			gravity.clearCheck = false;
+		}
+		else if (gravity.clearCheck == true && stageCount == 3)
+			SceneManager.LoadScene("LastScene"); 
+		if (textScene && Input.GetKeyDown(KeyCode.Space) || OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+		{
+			textScene = false;
+			if(stageCount == 0)
+				stageCount++;
+			StopAllCoroutines();
+			SoundManager.instance.StopKeyboard();
+			SceneManager.LoadScene(stageCount);
+		}
+	}
+	public void Text(int stageCount)
+    {
+		textScene = true;
+		story = "";		
+		txt = txt.GetComponent<Text>();
+		if(stageCount == 0) 
+		{
+			SoundManager.instance.BgmSingle(firstBgmSource);
+			txt.text = "현조는 나 \nNIS(국정원) 안에 서울에 내가 같혀 있었다.\n침이 날이 가뜩이나 서 있는 주사를 맞아버렸다.\n졸려서 찢고 싶은 잠이다." +
+			        	"\n세상이 나를 중심으로 궁그려 돌고 있다.\n내가 움직일수 있다.\n나는 이데아";
+		}
+		else if(stageCount == 1)
+        {
+			SoundManager.instance.BgmSingle(secondBgmSource);
+			txt.text = "1라운드 클리어";
+
+        }
+		else if(stageCount == 2)
+        {
+			SoundManager.instance.BgmSingle(thirdBgmSource);
+			txt.text = "2라운드 클리어";
+        }
+		story = txt.text;		
+		txt.text = "";
+		//TODO: add optional delay when to start
+		StartCoroutine("PlayText");	
+	}
+	IEnumerator PlayText()
+	{	
+		SoundManager.instance.KeyboardSingle(keyboardSource);
+		foreach (char c in story)
+		{
+			if (textScene)
+			{
+				txt.text += c;
+				yield return new WaitForSeconds(0.13f);
+			}
+		}
+		story = "";
+		//textScene = false;
+		SoundManager.instance.StopKeyboard();
+
+	}
+}
---
+++ SON/New Unity Project/Assets/Scripts/NoneVRPlayer.cs
@@ -0,0 +1,176 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngineInternal.XR.WSA;
+
+public class NoneVRPlayer : MonoBehaviour
+{  
+    //카메라 및 캐릭터 이동관련
+    [SerializeField] float cameraRotSpeed = 150f;
+    [SerializeField] float moveDIrection = 10f;
+    [SerializeField] float cameraMaxRot = 80;
+
+    float turnValue;
+    float moveValue;
+    //논리값
+    [SerializeField] public static bool clearCheck = false;
+    //[SerializeField] bool gravityOn;
+    //[SerializeField] bool deadCheck;
+    [SerializeField] bool turnCheck;
+    [SerializeField] bool movingCheck;
+    //그외
+    [SerializeField] Transform playerTr;
+    [SerializeField] float rotSpeed = 5f;
+    //[SerializeField] Enemy enemy;
+    float xC;
+    float yC;
+    float turnningCount = 0;
+    float movingCount = 0;
+    //필요 컴포넌트
+    [SerializeField] Camera camera;
+    public GameObject rightArmLight;
+    public GameObject leftArmLight;
+    public GameObject ground;
+    public AudioClip gravitySource, groundedSource, clearPointSource;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (clearCheck == false)
+        {
+            Moving();
+            CameraRot();
+            PlayerRot();
+            RotGround();
+        }
+        
+    }
+    void Moving()
+    {
+        #region Axis 무빙
+        //if (gravityOn == false && deadCheck == false)
+        //{
+        //    float moveDirX = Input.GetAxis("Horizontal");
+        //    float moveDirZ = Input.GetAxis("Vertical");
+
+        //    Vector3 moveHorizontal = transform.right * moveDirX;
+        //    Vector3 moveVertical = transform.forward * moveDirZ;
+        //    Vector3 velocity = (moveHorizontal + moveVertical).normalized * moveDIrection;
+        //    rb.MovePosition(transform.position + velocity * Time.deltaTime);
+        //}
+        #endregion
+        if (Input.GetKeyDown(KeyCode.W) && movingCheck == false)
+        {
+            moveValue = 1;
+            StartCoroutine(TriggerMoving());
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && movingCheck == false)
+        {
+            moveValue = -1;
+            StartCoroutine(TriggerMoving());
+        }
+    }
+    IEnumerator TriggerMoving()
+    {
+        movingCheck = true;
+        movingCount = 0.1f;
+        Vector3 moveDevelop = moveValue > 0 ? Vector3.forward : Vector3.back;
+        while (movingCount < moveDIrection)
+        {
+            transform.Translate(moveDevelop * movingCount * Time.deltaTime);
+            movingCount += 0.1f;
+            yield return null;
+        }
+        movingCheck = false;
+    }
+
+    void RotGround()
+    {
+        //SoundManager.instance.PlaySingle(gravitySource);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            turnValue = 1;          
+            if(turnCheck == false)
+                StartCoroutine(MapTurnning());
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            turnValue = -1;
+            if (turnCheck == false)
+                StartCoroutine(MapTurnning());
+        }
+    }
+
+    IEnumerator MapTurnning()
+    {
+        turnCheck = true;
+        //rb.useGravity = true;
+        turnningCount = 0;
+        Vector3 turnDevelop = turnValue > 0 ? Vector3.right : Vector3.left;
+        while (turnningCount < 90)
+        {
+            ground.transform.Rotate((turnDevelop), Space.Self);
+            turnningCount += 1;
+            yield return null;
+        }
+        turnCheck = false;
+    }
+    void PlayerRot()
+    {
+        float x = Input.GetAxis("Mouse X");
+        Vector3 follwing = new Vector3(0, x, 0) * cameraRotSpeed;
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(follwing));
+    }
+    void CameraRot()
+    { 
+        //상하 카메라 회전
+        float xRotation = Input.GetAxisRaw("Mouse Y");
+        float cameraRotationX = xRotation * cameraRotSpeed;
+        xC -= cameraRotationX; // 카메라 회전값을 감산해야 마우스 위치대로 회전
+        xC = Mathf.Clamp(xC, -cameraMaxRot, cameraMaxRot); //회전값을 최대치로 계산
+
+        camera.transform.localEulerAngles = new Vector3(xC, 0, 0);        
+    }
+    void PlayerDead()
+    {
+        //deadCheck = true;
+        Debug.Log("Hp = 0");
+    }
+    //private void OnCollisionEnter(Collider other)
+    //{
+    //    if (other.tag == "ClearBox")
+    //    {
+    //        if (Physics.Raycast(transform.position, Vector3.down, 5f, 1 << LayerMask.NameToLayer("ClearGround")))
+    //        {
+    //            //SoundManager.instance.PlaySingle(clearPointSource);
+    //            clearCheck = true;
+    //        }
+            
+    //    }
+    //    if(other.tag == "Ground")
+    //    {
+    //        rb.useGravity = false;
+    //    }
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.collider.tag == null)
+    //    {
+    //        rb.useGravity = true;
+    //    }
+    //}    
+    
+}
+    
