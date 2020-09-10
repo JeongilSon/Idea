ef6cd1e7519db9a85460e168acb8b7499130778f
Haps157 <69827171+Haps157@users.noreply.github.com>
2020-09-10 11:23:27 +0900

    New

---
+++ SON/New_Map/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSound;
+    public AudioSource bgmSound;
+    public AudioSource keyboardSound;
+    public static SoundManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    public void PlaySingle(AudioClip clip)
+    {
+        efxSound.clip = clip;
+        efxSound.Play();
+    }
+    public void BgmSingle(AudioClip clip)
+    {
+        bgmSound.clip = clip;
+        bgmSound.Play();
+    }
+    public void KeyboardSingle(AudioClip clip)
+    {
+        keyboardSound.clip = clip;
+        keyboardSound.Play();       
+    }
+    public void StopKeyboard()
+    {
+        keyboardSound.Stop();
+    }
+}
